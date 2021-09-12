2cc175352c524a0b25ec8fcea30e688c003ace4d
IdanHerman12 <idanh156@gmail.com>
2021-09-12 21:38:53 +0300

    completed few of the functions

--- scripts/index.js
+++ scripts/index.js
@@ -12,9 +12,10 @@ function playSong(songId) {
  * Creates a song DOM element based on a song object.
  */
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
-    const children = []
-    const classes = []
-    const attrs = { onclick: `playSong(${id})` }
+    const song=arguments[0]
+    const children = songList(song)
+    const classes = ["song"]
+    const attrs = { onclick: `playSong(${id})`,cursor:"pointer" }
     return createElement("div", children, classes, attrs)
 }
 
@@ -22,8 +23,9 @@ function createSongElement({ id, title, album, artist, duration, coverArt }) {
  * Creates a playlist DOM element based on a playlist object.
  */
 function createPlaylistElement({ id, name, songs }) {
-    const children = []
-    const classes = []
+    const playlist=arguments[0]
+    const children = playPlaylist(playlist)
+    const classes = ["playlist"]
     const attrs = {}
     return createElement("div", children, classes, attrs)
 }
@@ -41,16 +43,84 @@ function createPlaylistElement({ id, name, songs }) {
  * @param {Object} attributes - the attributes for the new element
  */
 function createElement(tagName, children = [], classes = [], attributes = {}) {
-    const element= document.createElement(tagName)
+    let element= document.createElement(tagName)
    classes.forEach(c =>element.classList.add(c))
    const attribute=Object.keys(attributes)
    for(let i=0;i<attribute.length;i++){
        element.setAttribute(attribute[i],attributes[attribute[i]])  
    }
    for(let i=0;i<children.length;i++){
-    element.textContent+=children[i] 
+    element.append(children[i])
+   }
+    return  element
 }
-   return element
+// You can write more code below this line
+
+function songList(song){
+const list=[]
+for(let key in song){
+     if(key.toString()!=='coverArt' && key.toString()!=='duration'){
+    const li=document.createElement('li');
+    li.innerText=`${key}: ${song[key]}`;
+    list.push(li)
+}
+ else if(key.toString()==="duration"){
+    const li=document.createElement('li');
+    let duration=convertDuriation(song[key])
+    duration.toString
+    li.innerText=`${key}: ${duration}`;
+    list.push(li)
+}
+else{
+    const img=document.createElement('img')
+    img.src=song[key]
+    list.push(img)
+}
+}
+return list
 }
 
-// You can write more code below this line
+function playPlaylist(playlist){
+    const list=[]
+    const sumDuration =playlistDuration(playlist)
+    for(let key in playlist){
+        if(key.toString()!=="songs"){
+           const li=document.createElement('li')
+               li.innerText=`${key}: ${playlist[key]}`;
+                  list.push(li)
+                }
+                else{
+                    const li=document.createElement("li")
+                    li.innerText=`number of songs: ${playlist.songs.length}`
+                    list.push(li)
+      
+                }
+    }
+    // let sumDuration=playlistDuration(playlist[key]);
+    const li=document.createElement("li")
+    li.innerText=`duration: ${sumDuration}`;
+    list.push(sumDuration)
+    return list 
+    }
+
+// function sumPlaylist(playlist){
+//     let sumDuration=0
+//  for(let i=0;i<playlist.length;i++){
+//      const duration=player.songs[exist(playlist[i],player.songs)].duration
+//          sumDuration+=duration
+//      }
+//  return convertDuriation(sumDuration)
+// }
+
+
+
+
+const x=document.getElementById("songs")
+for(let i=0;i<player.songs.length;i++){
+x.appendChild(createSongElement(player.songs[i]))
+}
+
+const y=document.getElementById("playlists")
+for(let i=0;i<player.playlists.length;i++){
+    y.appendChild(createPlaylistElement(player.playlists[i]))
+    }
