527cab47f0a2980cb89d12fd20b0bccb5378434f
Dayan135 <dayan1357999@gmail.com>
2021-09-12 16:08:49 +0300

    start
    generating the page

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,61 @@
+function convertDuration(duration) {
+    //parameters - duration (type string('mm:ss') / number(seconds))
+    //return string('mm:ss') if number was given
+    //return number(seconds) if string was given
+    if(typeof(duration) === 'number'){
+        let min  = Math.floor(duration / 60);
+        let sec = duration % 60;
+        if (min < 10) {
+        min = "0" + String(min);
+        }
+        if (sec < 10) {
+        sec = "0" + String(sec);
+        }
+        return min + ':' + sec;
+    }
+    else{//if its a string
+        return parseInt(duration.slice(3)) + parseInt(duration.slice(0,2)) * 60;
+    }
+}
+
+function getEl(arr, id){
+    //parameters - arr (type array(of objects))
+    //             id (type number)
+    //returns the object in the array that has the wanted id.
+    for(let el of arr){
+      if(el.id === id){
+        return el;
+      }
+    }
+    throw("couldn't find element where id=" + id);
+  }
+
+function newWindow(){
+    let songEl = document.getElementById("songs")
+    while(songEl.firstChild){
+        songEl.removeChild(songEl.firstChild);
+    }
+
+    let plEl = document.getElementById("playlists")
+    while(plEl.firstChild){
+        plEl.removeChild(plEl.firstChild);
+    }
+}
+
+function songsHandler(){
+    newWindow()
+    for(const song of player.songs){
+        document.getElementById("songs").appendChild(createSongElement(song))
+    }
+}
+
+function playlistsHandler(){
+    newWindow()
+    for(const playlist of player.playlists){
+        document.getElementById("playlists").appendChild(createPlaylistElement(playlist))
+    }
+}
+
 /**
  * Plays a song from the player.
  * Playing a song means changing the visual indication of the currently playing song.
@@ -13,8 +71,23 @@ function playSong(songId) {
  */
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
     const children = []
-    const classes = []
+    const classes = ["song"]
     const attrs = { onclick: `playSong(${id})` }
+
+    const topic = document.createElement("h1");
+    topic.textContent = "song #" + id + "\n";
+    children.push(topic)
+
+    const info = document.createElement("p");
+    info.textContent = "name: " + title + "\nalbum: " + album + "\nartist: " + artist + "\nduration: " + convertDuration(duration);
+    children.push(info)
+
+    const img = document.createElement("img");
+    img.src = coverArt;
+    children.push(img)
+
+    //createElement('div',children,classes,attrs)
+    //document.getElementById("songs").appendChild(createElement("div", children, classes, attrs));
     return createElement("div", children, classes, attrs)
 }
 
@@ -23,8 +96,23 @@ function createSongElement({ id, title, album, artist, duration, coverArt }) {
  */
 function createPlaylistElement({ id, name, songs }) {
     const children = []
-    const classes = []
+    const classes = ["playlist"]
     const attrs = {}
+    
+    const topic = createElement("header");
+    topic.textContent = "playlist #" + id + "  -  " + name;
+    children.push(topic)
+
+    const info = createElement("p");
+    info.textContent = "songs -";
+    children.push(info)
+    for(let i = 1; i <= songs.length; i++){
+
+        children.push(createSongElement(getEl(player.songs,songs[i-1])));
+    }
+
+    console.log(children);
+    
     return createElement("div", children, classes, attrs)
 }
 
@@ -41,7 +129,19 @@ function createPlaylistElement({ id, name, songs }) {
  * @param {Object} attributes - the attributes for the new element
  */
 function createElement(tagName, children = [], classes = [], attributes = {}) {
-    // Your code here
+    let newEl = document.createElement(tagName);
+    for(const child of children){
+        newEl.appendChild(child);
+    }
+    for(const cls of classes){
+        newEl.classList.add(cls);
+    }
+    newEl.attributes = attributes;
+    //document.getElementById("songs").appendChild(newEl);
+    return newEl
 }
 
 // You can write more code below this line
+for(const song of player.songs){
+    document.getElementById("songs").appendChild(createSongElement(song));
+}
