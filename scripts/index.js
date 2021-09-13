ec4bdca74d37d607c2034a69b2c2852841084b7d
amitaibat97 <amitaibar97@gmail.com>
2021-09-13 17:41:25 +0300

    playSong element with setTimeout
    marks the playing song and ends when it ends.

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,9 @@
 
 
 function playSong(songId) {
-
+    const currentSong = document.getElementById("song" + songId);
+    currentSong.classList.add("now-playing");
+    let endSong = setTimeout(() => currentSong.classList.remove("now-playing"), getSongUsingId(songId).duration * 1000);
 
 }
 
@@ -16,8 +18,8 @@ function playSong(songId) {
  * Creates a song DOM element based on a song object.
  */
 
- 
-    
+
+
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
     const children = [
         createElement("img", [], ["coverArt"], {
@@ -27,22 +29,23 @@ function createSongElement({ id, title, album, artist, duration, coverArt }) {
         createElement("strong", [title]),
         createElement("span", [", album: ", album], ["album"]),
         createElement("span", [", artist: ", artist], ["artist"]),
-        createElement("span", [", " , mmss(duration)], ["duration"]),
+        createElement("span", [", ", mmss(duration)], ["duration"]),
+
     ]
     const classes = []
-    const attrs = { onclick: `playSong(${id})` }
-    return createElement("div", children, classes, attrs)
+
+    return createElement("div", children, classes, { onclick: `playSong(${id})`, id: "song" + id })
 }
 
 /**
  * Creates a playlist DOM element based on a playlist object.
  */
 function createPlaylistElement({ id, name, songs }) {
-    let numOfSongs= songs.length
+    let numOfSongs = songs.length
     const children = [
         createElement("p", ["Playlist Name: ", name], ["name"]),
         createElement("span", [" number of songs: ", numOfSongs], ["length"]),
-        createElement("span", [", sum duration: ",mmss(playlistDuration(id))], ["duration"] )
+        createElement("span", [", sum duration: ", mmss(playlistDuration(id))], ["duration"])
     ]
     const classes = []
     const attrs = {}
@@ -64,7 +67,7 @@ function createPlaylistElement({ id, name, songs }) {
 
 
 
- function createElement(tagName, children = [], classes = [], attributes = {}) {
+function createElement(tagName, children = [], classes = [], attributes = {}) {
     let elementName = document.createElement(tagName);
     //children//
     for (value of children) {
@@ -84,56 +87,57 @@ function createPlaylistElement({ id, name, songs }) {
 
 player.songs.forEach((song) => {
     document.querySelector("#songs").append(createSongElement(song));
- });
+});
 
- player.playlists.forEach((playlist) => {
+player.playlists.forEach((playlist) => {
     document.querySelector("#playlists").append(createPlaylistElement(playlist));
- });
+});
 
 
- function mmss(duration) {
+function mmss(duration) {
     let arr = [];
     let minutes = Math.floor(duration / 60);
     let seconds = duration % 60;
     arr.push(minutes);
     arr.push(seconds);
     if (minutes < 10) {
-      arr[0] = "0" + arr[0];
+        arr[0] = "0" + arr[0];
     }
     if (seconds < 10) {
-      arr[1] = "0" + arr[1];
+        arr[1] = "0" + arr[1];
     }
-  
+
     arr = arr.join(":");
     return arr;
-  }
+}
 
-  function getSongUsingId(id) {
+function getSongUsingId(id) {
     let n;
     for (let i = 0; i < player.songs.length; i++) {
-      if (player.songs[i].id === id) {
-        currentSong = player.songs[i];
-        return currentSong;
-      }
+        if (player.songs[i].id === id) {
+            currentSong = player.songs[i];
+            return currentSong;
+        }
     }
 }
 
-  function getPlaylistLocationUsingID(id) {
+function getPlaylistLocationUsingID(id) {
     for (let n in player.playlists) {
-      if (id === player.playlists[n].id) {
-        return n;
-      }
+        if (id === player.playlists[n].id) {
+            return n;
+        }
     }
-  }
+}
 
-  function playlistDuration(id) {
+function playlistDuration(id) {
     let sumDurationOfSongs = 0;
     let playlistLocation = getPlaylistLocationUsingID(id);
     let songsInPlaylist = player.playlists[playlistLocation].songs;
     for (let n of songsInPlaylist) {
-      sumDurationOfSongs += getSongUsingId(n).duration;
+        sumDurationOfSongs += getSongUsingId(n).duration;
     }
     return sumDurationOfSongs;
-  }
+}
 
 
+console.log(document.getElementById(7))
