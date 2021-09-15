4684e39e315a25825a6c7ed0231082e07006bd91
romans1995 <cboby26@gmail.com>
2021-09-15 15:29:41 +0300

    added color to the duration

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,62 @@
 // converting duration 
 const convertDuration = (duration) => {
 
-    let min = String(Math.floor(duration / 60));
-    let sec = String(duration % 60);
-  
-    min < 10 ? (min = '0' + String(min)) : min
-    sec < 10 ? (sec = '0' + String(sec)) : sec
-  
-    return min + ':' + sec
+  const minutes = Math.floor(duration / 60);
+  let seconds = duration % 60;
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
   }
- 
-    const Timer = (id) =>{
-      setTimeout(function(){playSong(id+1) }, 3000);
-    }
   
- async function playSong(id) {
-  document.getElementById(id).style.backgroundColor = "green";
-  setTimeout(function(){document.getElementById(id).style.backgroundColor = "white"  }, 3000);
-  await Timer(id);
-}
+  return `${minutes}:${seconds}`;
+  }
 
+  let timerInterval = null;
 
+  document.getElementsByClassName("duration").innerHTML = `...`;
+ 
+  
+  // function startTimer() {
+  //   timerInterval = setInterval(() => {
+  //     timePassed = timePassed += 1;
+  //     timeLeft = TIME_LIMIT - timePassed;
+  //     document.getElementById("short-duration").innerHTML = formatTime(timeLeft);
+  //   }, 1000);
+  // }
+    
+  
+ 
+
+    let runLoop;
+    
+
+     function playSong(id,newDuration) {
+      timerInterval = setInterval(() => {
+        timePassed = timePassed += 1;
+        timeLeft = TIME_LIMIT - timePassed;
+        document.getElementById("short-duration").innerHTML = formatTime(timeLeft);
+      }, 1000)
+      const TIME_LIMIT = newDuration;
+      let timePassed = 0;
+      let timeLeft = TIME_LIMIT;
+      if(id === 8){          //need to be set as length of all ids
+        id = 1;
+      }
+    
+      document.getElementById(id).style.backgroundColor = "green"; 
+      if(document.getElementById(id).style.backgroundColor = "green"){
+        document.getElementsByClassName('duration').style.color = "red";
+      }
+      runLoop = setTimeout(function(){document.getElementById(id).style.backgroundColor = "white" }, 3000); 
+
+      
+      let time = setTimeout(function(){playSong(id+1) }, 3000);
+     
+
+    
+        ///needs to set as duration song
+       
+       }
+   
 
 //  gives platlist duration 
 function playlistDuration(id) {
@@ -32,19 +68,24 @@ function playlistDuration(id) {
   }
 
 function createSongElement({ id,artist,duration, coverArt }) {
+  const  colored = (duration) => {
+    return duration < 600 ? 'color:green':'color:red';
+  } 
+    const classes = [];
     const getId = createElement('p',[id]);
     const artistEl = createElement("p", [artist]);
-    const durationEl = createElement("p", ["" + convertDuration(duration)] ,["duration", "short-duration"], {onclick: `console.log('${duration}')`});
+    let newDuration = convertDuration(duration)
+    const durationEl = createElement("p", [ newDuration] ,["duration", "short-duration"],{style:colored(duration)}, {onclick: `console.log('${duration}')`});
     const coverImageArtUrl = coverArt;
     const imgEl = createElement("img", [] ,["album-art"], {src: coverImageArtUrl});
-    const attrs = { onclick: `playSong(${id})`,id}
-
+    const attrs = { onclick: `playSong(${id})`,id,class:`songs`}
+    classes.push("songs");
     return createElement("div",[getId,"Artist: ", artistEl, "Duration: ", durationEl, imgEl],[],attrs);
   }
 
 
 function createPlaylistElement({id,name, songs = []} ) {
-    const children = [];ד
+    const children = [];
     const classes = [];
     const attrs = {};
     const idEl = createElement("span", ["" + id] ,["id"]);
@@ -54,7 +95,7 @@ function createPlaylistElement({id,name, songs = []} ) {
     classes.push("playlist")
     children.push(idEl,"Play list Name: ",playListName,"Songs: ", PLsongs," Duration: ", durationEl);
     
-    return createElement( "div",children, classes, attrs,id)
+    return createElement( "div",children, classes, attrs,id);
     
 
 }
@@ -93,7 +134,7 @@ const printAllSongs = (id) => {
     const songPrint = document.getElementById("songs");
 
     for(let song of player.songs){
-        const { title, album, artist,duration,id,coverArt} = song;
+        const { title, album, artist,duration = duration,id,coverArt} = song;
         const songElemnt = createSongElement({id, title, album, artist, duration, coverArt});
         songPrint.appendChild(songElemnt);
     }
