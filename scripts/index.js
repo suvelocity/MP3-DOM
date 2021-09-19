277ade5e3a824309877d3033a3d3fb7b08f75b49
MaorKurztag21 <rnrwznr@gmail.com>
2021-09-19 04:48:06 +0300

    Final repair of first mission which was done wrongly
    Change of all files

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,50 @@
 /**
+ * 
  * Plays a song from the player.
  * Playing a song means changing the visual indication of the currently playing song.
  *
  * @param {String} songId - the ID of the song to play
  */
  function playSong(songId) {
-   let song = getElementById(songId).style.backgroundColor = "yellow";
-      }
-  
-
+  let notChosens = document.getElementsByClassName('songShell');
+  for(let notChosen of notChosens){
+      notChosen.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }
+  document.getElementById(songId).style.backgroundColor ="hsla(50, 33%, 25%, .75)";
+}
 
 /**
 * Creates a song DOM element based on a song object.
 */
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
-   const song=arguments[0];
-   const children = songsList(song);
-   const classes = ["song"];
-   const attrs = { onclick: playSong(id),cursor:"pointer",id:id };
-   return document.createElement("div", children, classes, attrs);
+      let SongTitle = createElement('h2',[title],['songTitles']);
+      let songAlbum = createElement('h3',["Album: " + album]);
+      let songArtist = createElement('h4',["Artist: " + artist]);
+      let songDuration = createElement('h4',[secondsToMinutesConvertor(duration)]);
+      let songCoverArt  = createElement('img',[],[],{src: coverArt})
+      let songElement = createElement('div',[SongTitle, songAlbum, songArtist, songDuration, songCoverArt],['songShell'],{id: id});
+      console.log(songElement);
+      songElement.setAttribute('onclick', `playSong(${id})`)
+      return songElement;
+  }
+for(let song of player.songs){
+  let songsDiv = document.getElementById('songs');
+  songsDiv.append(createSongElement(song));
 }
-
 /**
 * Creates a playlist DOM element based on a playlist object.
 */
 function createPlaylistElement({ id, name, songs }) {
-   const playlist=arguments[0];
-   const children = playPlaylist(playlist);
-   const classes = ["playlist"];
-   const attrs = {};
-   return document.createElement("div", children, classes, attrs);
+  let playlistName = createElement('h2',[name]);
+  let playlistSongs = createElement('h3',["Amount of songs: " + songs.length]);
+  let playlistFulllDuration = createElement('h3',["Duration - " + playlistDuration(id)]);
+  let  playlistElem = createElement('div',[playlistName, playlistSongs, playlistFulllDuration],['playlistShell']);
+  return playlistElem;
 }
-
+for(let playlist of player.playlists){
+  let playlistList = document.getElementById('playlists');
+   playlistList.append(createPlaylistElement(playlist));
+}
 /**
 * Creates a new DOM element.
 *
@@ -45,73 +58,17 @@ function createPlaylistElement({ id, name, songs }) {
 * @param {Object} attributes - the attributes for the new element
 */
 function createElement(tagName, children = [], classes = [], attributes = {}) {
-   const newElement= document.createElement(tagName);
-   for(let i in classes){
-      newElement.classList+=i;
-   }
-   const attr=Object.keys(attributes);
-   for(let prop in attr.length){
-       newElement.setAttribute(attr[prop],attributes[attr[prop]]); 
-   }
-   for(let child in children.length){
-    newElement.textContent+=children[i];
+let newEl = document.createElement(tagName);
+for(let child of children){
+     newEl.append(child);
 }
-   return newElement
+for(let cls of classes){
+    newEl.classList.add(cls);
 }
+for(let attr in attributes){
+    newEl.setAttribute(attr, attributes[attr]);
+}
+return newEl
+}
+createSongElement('h2', [player.songs[0].title], "songTitles");
 
-// You can write more code below this line
-function songsList(song){
-   const listSongs=[]
-   for(let i in song){
-        if(i.toString()!=='duration' && i.toString()!=='coverArt'){
-       const liTag=document.createElement('li');
-       liTag.innerText=i+":"+ song[i]; 
-       listSongs.push(liTag)
-        }
-        else if(i.toString()==="duration"){
-         let liTag=document.createElement('li');
-         let duration=mmsstDuriation(song[i]);
-         liTag.innerText=i+":"+duration; 
-         list.push(liTag)
-     }
-     else{
-         const img=document.createElement('img')
-         img.src=song[i]
-         list.push(img)
-     }
-     }
-     return list
-   }
-
-
-
-   function playPlaylist(playlist){
-      const listPlaylists=[]
-      const sumDuration =playlistDuration(playlist)
-      for(let key in playlist){
-          
-          if(key.toString()!=="songs"){
-             let liTag=document.createElement('li')
-             liTag.innerText= key+" : "+playlist[key];
-             listPlaylists.push(liTag)
-                  }
-          else{
-              let liTag=document.createElement("li")
-              liTag.innerText="number of songs: "+playlist.sosgs.length; 
-             listPlaylists.push(liTag)
-                  }
-               }
-        let liTag=document.createElement("li")
-        liTag.innerText=duration+":"+sumDuration; `duration: ${sumDuration}`;
-        list.push(sumDuration)
-        return list 
-            }
-
-            const song=document.getElementById("songs");
-            for(let i in player.songs){
-               song.appendChild(createSongElement(player.songs[i]));
-            }
-            const playlist=document.getElementById("playlists");
-            for(let i in player.playlists){
-               playlist.appendChild(createPlaylistElement(player.playlists[i]))
-            }
